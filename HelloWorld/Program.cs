c3be39322567980ed8b3f62435a264fd2b217561
druszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2023-11-27 20:54:00 +0100

    Add some info about HDD

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,5 +5,6 @@
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello 2");
         Console.WriteLine("3");
+        Console.WriteLine("wdc pc sn520 sdapnuw-512g-1014");
     }
 }
