606f628bb27ba75130aaf73577ad18a7f6a0e0eb
druszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2023-11-27 14:06:23 +0100

    Modified Program - added new WriteLine Added Test class - empty.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello 2");
+        Console.WriteLine("Kolejna linia");
     }
 }
